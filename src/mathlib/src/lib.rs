874c0d537c78b53da4c818bd124901a997f043c3
pepega007xd <brablect@gmail.com>
2023-03-22 18:21:08 +0100

    Implemented the math library.

--- src/mathlib/src/lib.rs
+++ src/mathlib/src/lib.rs
@@ -20,7 +20,57 @@ impl MathExpr {
     /// Evaluates given expression and returns `Some` of value
     /// or `None`, if expression is invalid (such as division by zero)
     pub fn eval(&self) -> Option<Decimal> {
-        None
+        match self {
+            MathExpr::AddExpr(terms) => terms
+                .iter()
+                .map(|term| term.eval())
+                .reduce(|a, b| {
+                    if let (Some(a), Some(b)) = (a, b) {
+                        a.checked_add(b)
+                    } else {
+                        None
+                    }
+                })
+                .flatten(),
+
+            MathExpr::MulExpr(terms) => terms
+                .iter()
+                .map(|term| term.eval())
+                .reduce(|a, b| {
+                    if let (Some(a), Some(b)) = (a, b) {
+                        a.checked_mul(b)
+                    } else {
+                        None
+                    }
+                })
+                .flatten(),
+
+            MathExpr::DivExpr(a, b) => {
+                let (a, b) = (a.eval(), b.eval());
+                if let (Some(a), Some(b)) = (a, b) {
+                    a.checked_div(b)
+                } else {
+                    None
+                }
+            }
+
+            MathExpr::PowExpr(a, b) => {
+                let (a, b) = (a.eval(), b.eval());
+                if let (Some(a), Some(b)) = (a, b) {
+                    // We need to make sure the operation make sense
+                    // (no fractional exponents on negatice numbers).
+                    if a.is_sign_negative() && b != b.round() {
+                        None
+                    } else {
+                        a.checked_powd(b)
+                    }
+                } else {
+                    None
+                }
+            }
+
+            MathExpr::Const(a) => Some(*a),
+        }
     }
 }
 
@@ -54,7 +104,7 @@ mod tests {
             const_val("-42_000"),
             const_val("78.58"),
         ]);
-        let expr = AddExpr(vec![const_val("14"), inner, const_val("78.58")]);
+        let expr = AddExpr(vec![const_val("14"), inner, const_val("-0.666")]);
         assert_eq!(expr.eval(), Some(dec!(-118912.086)));
     }
 
@@ -82,8 +132,8 @@ mod tests {
 
     #[test]
     fn division_by_zero() {
-        // -4466.7 / ( 0 / -7.3) == 336.1537 (4 decimal places)
-        let divisor = DivExpr(box_const("97"), box_const("-7.3"));
+        // -4466.7 / ( 0 / -7.3) == None
+        let divisor = DivExpr(box_const("0"), box_const("-7.3"));
         assert_eq!(divisor.eval(), Some(dec!(0)));
         let expr = DivExpr(box_const("-4466.7"), Box::new(divisor));
         assert_eq!(expr.eval(), None);
@@ -108,11 +158,33 @@ mod tests {
 
     #[test]
     fn exponentiation_invalid() {
-        // -3 ** (4 ** -2) == 1.07317 (5 decimal places)
-        let exp = PowExpr(box_const("7.8"), box_const("-1.4"));
-        assert_eq!(exp.eval(), Some(dec!(0.25)));
-        let expr = PowExpr(box_const("-3.5"), Box::new(exp));
-        let value = expr.eval().unwrap();
-        assert_eq!(format!("{value:.5}"), "1.07317");
+        // -1 ** 0.5 == None
+        let expr = PowExpr(box_const("-1"), box_const("0.5"));
+        assert_eq!(expr.eval(), None);
+    }
+
+    #[test]
+    fn large_expression() {
+        // -1 * (-998 + 45 ** (44.7 - 44.125)) /
+        // (3.2 * 63.004 * (78.44 ** -8 - 34.996)) == 0.142712 (6 decimal digits)
+        let first_subexpr = PowExpr(
+            box_const("45"),
+            Box::new(AddExpr(vec![const_val("44.7"), const_val("-44.125")])),
+        );
+        let first_expr = MulExpr(vec![
+            const_val("-1"),
+            AddExpr(vec![const_val("998"), first_subexpr]),
+        ]);
+
+        let second_subexpr = AddExpr(vec![
+            const_val("-34.996"),
+            PowExpr(box_const("78.44"), box_const("-8")),
+        ]);
+        let second_expr = MulExpr(vec![const_val("3.2"), const_val("63.004"), second_subexpr]);
+
+        let value = DivExpr(Box::new(first_expr), Box::new(second_expr))
+            .eval()
+            .unwrap();
+        assert_eq!(format!("{value:.6}"), "0.142712");
     }
 }
