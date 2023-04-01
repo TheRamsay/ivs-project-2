63426a4e2b38ccf9d261be6bf8c3123ac7c31ba3
pepega007xd <brablect@gmail.com>
2023-04-02 00:22:53 +0200

    "Added binary addition, subtraction, and multiplication expressions"

--- src/mathlib/src/lib.rs
+++ src/mathlib/src/lib.rs
@@ -7,10 +7,11 @@ use rust_decimal::prelude::*;
 /// on the stack.
 ///
 pub enum MathExpr {
-    /// AddExpr also represents subtraction, just
-    /// add negative number
-    AddExpr(Vec<MathExpr>),
-    MulExpr(Vec<MathExpr>),
+    AddExpr(Box<MathExpr>, Box<MathExpr>),
+    SubExpr(Box<MathExpr>, Box<MathExpr>),
+    MulExpr(Box<MathExpr>, Box<MathExpr>),
+    AddExprMultiple(Vec<MathExpr>),
+    MulExprMultiple(Vec<MathExpr>),
     DivExpr(Box<MathExpr>, Box<MathExpr>),
     PowExpr(Box<MathExpr>, Box<MathExpr>),
     Const(Decimal),
@@ -21,7 +22,34 @@ impl MathExpr {
     /// or `None`, if expression is invalid (such as division by zero)
     pub fn eval(&self) -> Option<Decimal> {
         match self {
-            MathExpr::AddExpr(terms) => terms
+            MathExpr::AddExpr(a, b) => {
+                let (a, b) = (a.eval(), b.eval());
+                if let (Some(a), Some(b)) = (a, b) {
+                    a.checked_add(b)
+                } else {
+                    None
+                }
+            }
+
+            MathExpr::SubExpr(a, b) => {
+                let (a, b) = (a.eval(), b.eval());
+                if let (Some(a), Some(b)) = (a, b) {
+                    a.checked_sub(b)
+                } else {
+                    None
+                }
+            }
+
+            MathExpr::MulExpr(a, b) => {
+                let (a, b) = (a.eval(), b.eval());
+                if let (Some(a), Some(b)) = (a, b) {
+                    a.checked_mul(b)
+                } else {
+                    None
+                }
+            }
+
+            MathExpr::AddExprMultiple(terms) => terms
                 .iter()
                 .map(|term| term.eval())
                 .reduce(|a, b| {
@@ -33,7 +61,7 @@ impl MathExpr {
                 })
                 .flatten(),
 
-            MathExpr::MulExpr(terms) => terms
+            MathExpr::MulExprMultiple(terms) => terms
                 .iter()
                 .map(|term| term.eval())
                 .reduce(|a, b| {
@@ -90,29 +118,64 @@ mod tests {
 
     #[test]
     fn one_plus_one() {
+        let a = box_const("1");
+        let b = box_const("1");
+        let c = AddExpr(a, b);
+        assert_eq!(c.eval(), Some(dec!(2)));
+    }
+
+    #[test]
+    fn one_plus_one_multiple() {
         let a = const_val("1");
         let b = const_val("1");
-        let c = AddExpr(vec![a, b]);
+        let c = AddExprMultiple(vec![a, b]);
         assert_eq!(c.eval(), Some(dec!(2)));
     }
 
     #[test]
     fn addition_subtraction() {
         // (14 + (-77004 - 42000 + 78.58) - 0.666) == -118912.086
-        let inner = AddExpr(vec![
+        let inner = AddExpr(
+            Box::new(AddExpr(box_const("-77_004"), box_const("-42_000"))),
+            box_const("78.58"),
+        );
+        let expr = AddExpr(
+            Box::new(AddExpr(box_const("14"), Box::new(inner))),
+            box_const("-0.666"),
+        );
+        assert_eq!(expr.eval(), Some(dec!(-118912.086)));
+    }
+
+    #[test]
+    fn addition_subtraction_multiple() {
+        // (14 + (-77004 - 42000 + 78.58) - 0.666) == -118912.086
+        let inner = AddExprMultiple(vec![
             const_val("-77_004"),
             const_val("-42_000"),
             const_val("78.58"),
         ]);
-        let expr = AddExpr(vec![const_val("14"), inner, const_val("-0.666")]);
+        let expr = AddExprMultiple(vec![const_val("14"), inner, const_val("-0.666")]);
         assert_eq!(expr.eval(), Some(dec!(-118912.086)));
     }
 
     #[test]
     fn multiplication() {
         // (-4.35 * (0.44 * 80) * -78.5 * -0.5) == 6009.96
-        let inner = MulExpr(vec![const_val("0.44"), const_val("80")]);
-        let expr = MulExpr(vec![
+        let inner = MulExpr(box_const("0.44"), box_const("80"));
+        let expr = MulExprMultiple(vec![
+            const_val("4.35"),
+            inner,
+            const_val("-78.5"),
+            const_val("-0.5"),
+        ]);
+        assert_eq!(expr.eval(), Some(dec!(6009.96)));
+    }
+
+    #[test]
+    fn multiplication_multiple() {
+        // (-4.35 * (0.44 * 80) * -78.5 * -0.5) == 6009.96
+        let inner = MulExprMultiple(vec![const_val("0.44"), const_val("80")]);
+        let expr = MulExprMultiple(vec![
             const_val("4.35"),
             inner,
             const_val("-78.5"),
@@ -169,22 +232,26 @@ mod tests {
         // (3.2 * 63.004 * (78.44 ** -8 - 34.996)) == 0.142712 (6 decimal digits)
         let first_subexpr = PowExpr(
             box_const("45"),
-            Box::new(AddExpr(vec![const_val("44.7"), const_val("-44.125")])),
+            Box::new(AddExprMultiple(vec![
+                const_val("44.7"),
+                const_val("-44.125"),
+            ])),
         );
-        let first_expr = MulExpr(vec![
-            const_val("-1"),
-            AddExpr(vec![const_val("998"), first_subexpr]),
-        ]);
+        let first_expr = MulExpr(
+            box_const("-1"),
+            Box::new(AddExpr(box_const("998"), Box::new(first_subexpr))),
+        );
 
-        let second_subexpr = AddExpr(vec![
+        let second_subexpr = AddExprMultiple(vec![
             const_val("-34.996"),
             PowExpr(box_const("78.44"), box_const("-8")),
         ]);
-        let second_expr = MulExpr(vec![const_val("3.2"), const_val("63.004"), second_subexpr]);
+        let second_expr =
+            MulExprMultiple(vec![const_val("3.2"), const_val("63.004"), second_subexpr]);
 
         let value = DivExpr(Box::new(first_expr), Box::new(second_expr))
             .eval()
             .unwrap();
-        assert_eq!(format!("{value:.6}"), "0.142712");
+        assert_eq!(format!("{value:.6?}"), "0.142712");
     }
 }
