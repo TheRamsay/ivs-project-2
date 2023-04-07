3c51fa984767b75a7e3a1d533c7cea2b4f6d09fe
Dominik Huml <ramsayofbolton@gmail.com>
2023-04-07 13:47:11 +0200

    backup

--- src/backend/src/main.rs
+++ src/backend/src/main.rs
@@ -3,8 +3,15 @@
     windows_subsystem = "windows"
 )]
 
+use tauri::{Manager, PhysicalSize};
+
 fn main() {
     tauri::Builder::default()
+        .setup(|app| {
+            let main_window = app.get_window("main").expect("Couldn't get the main window.");
+            main_window.set_size(PhysicalSize { height: 1100, width: 700 }).expect("Couldn't change the windows dimensions.");
+            Ok(())
+        })
         .invoke_handler(tauri::generate_handler![hello])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
