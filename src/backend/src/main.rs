2bdb9e900628a135ac350805453f7eb57d2930c1
pepega007xd <brablect@gmail.com>
2023-04-25 21:47:22 +0200

    added documentation for backend and mathlib

--- src/backend/src/main.rs
+++ src/backend/src/main.rs
@@ -1,3 +1,7 @@
+//! This is the backend of the calculator.
+//! It also handles parsing of mathematical expressions, which is implemented
+//! in the module [parser].
+
 #![cfg_attr(
     all(not(debug_assertions), target_os = "windows"),
     windows_subsystem = "windows"
@@ -8,6 +12,8 @@ use tauri::{Manager, PhysicalSize};
 mod parser;
 use parser::parse_input;
 
+/// This function is the main entry point to the calculator app. It sets up the Tauri
+/// runtime, and registers the `parse_and_eval` function to be used from the frontend.
 fn main() {
     tauri::Builder::default()
         .setup(|app| {
@@ -27,6 +33,10 @@ fn main() {
         .expect("error while running tauri application");
 }
 
+/// This function parses the input string, evaluates the resulting expression,
+/// and returns the output as a string.
+/// The output is returned as a Result containing either the result
+/// or an error message in case of parsing or evaluation errors.
 #[tauri::command]
 fn parse_and_eval(input: &str) -> Result<String, String> {
     Ok(parse_input(input)
