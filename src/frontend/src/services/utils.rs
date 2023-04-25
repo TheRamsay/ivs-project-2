3ace4cb2b87a2213cf7f2da20b934d22479a65b9
Dominik Huml <ramsayofbolton@gmail.com>
2023-04-26 01:12:23 +0200

    Docs for utils module

--- src/frontend/src/services/utils.rs
+++ src/frontend/src/services/utils.rs
@@ -1,4 +1,7 @@
-pub const ALLOWED_KEYS: &'static [&str; 27] = &[
+//! Helper functions, mainly for input validation
+
+/// List of allowed input keys
+pub const ALLOWED_KEYS: &'static [&str; 28] = &[
     "backspace",
     "enter",
     "+",
@@ -25,9 +28,11 @@ pub const ALLOWED_KEYS: &'static [&str; 27] = &[
     "6",
     "7",
     "8",
-    "9"
+    "9",
+    "0"
 ];
 
+/// Remaps button signs to real expression symbols
 pub fn remap_keyboard_signs(value: &str) -> Vec<&str> {
     match value {
         "*" => vec!["×"],
@@ -39,16 +44,18 @@ pub fn remap_keyboard_signs(value: &str) -> Vec<&str> {
     }
 }
 
+/// Checks if key is allowed input
 pub fn is_legal_key(key: &str) -> bool {
     ALLOWED_KEYS.contains(&key)
 }
 
-// Checks if string is legal operator
+/// Checks if string is legal operator
 pub fn is_operator(value: &String) -> bool {
     let operators = vec!["+", "-", "×", "/", "!", "ln", "^", "√"];
     operators.iter().any(|&o| &*value == o)
 }
 
+/// Checks if string is valid number (only numeric characters or single dot followed by numeric characters)
 pub fn is_number(value: &String) -> bool {
     value.len() > 0 && value.chars().all(|c| c == '.' || c.is_numeric())
 }
